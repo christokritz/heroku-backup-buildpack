de8559bb2f16c7919b0747eac6bc865b2d0b33de
Christo Kritzinger <christo.kritzinger@shareforce.co.za>
2023-04-20 16:16:14 +0100

    updated

--- example.sh
+++ example.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo starting process 123
+echo "$DATABASE_URL"
 aws_command="aws"
-${aws_command} s3 cp example.sh s3://heroku-backups-test/abc/example.sh
+#${aws_command} s3 cp example.sh s3://heroku-backups-test/abc/example.sh
