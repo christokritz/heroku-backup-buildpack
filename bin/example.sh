7bfb74f5b80102c078fa4776f5715ff0ce36affb
Christo Kritzinger <christo.kritzinger@shareforce.co.za>
2023-04-20 16:09:26 +0100

    aws added

--- bin/example.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo starting process 1234
--- example.sh
+++ example.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo starting process 123
+aws_command="aws"
+${aws_command} s3 cp example.sh s3://heroku-backups-test/abc/example.sh
