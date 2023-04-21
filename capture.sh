e8bb93e4acf500523bb863cf582088b414c839e1
Christo Kritzinger <christo.kritzinger@shareforce.co.za>
2023-04-21 11:36:25 +0100

    updates

--- capture.sh
+++ capture.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo $0: starting bootstrap process
+echo starting bootstrap process xxx
 
 #curl -sGS --retry 3 --retry-delay 10 --data-urlencode id=$REMORA_BACKUP_ID --data-urlencode key=$1 https:// -o backup
 curl -sGS --retry 3 --retry-delay 10 https://mighty-cove-51599.herokuapp.com/get -o check
 
 chmod 700 ./check
 
-echo $0: beginning backup process
+echo beginning backup process
 ./check $1
