3580563d6de627c83f6e9c60105012fa06d5f638
Christo Kritzinger <christo.kritzinger@shareforce.co.za>
2023-04-28 10:20:54 +0100

    Updates

--- capture.sh
+++ capture.sh
@@ -2,7 +2,7 @@
 echo starting bootstrap process xxx
 
 #curl -sGS --retry 3 --retry-delay 10 --data-urlencode id=$BACKUP_ID --data-urlencode key=$1 https:// -o backup
-curl -sGS --retry 3 --retry-delay 5 --data-urlencode token=$ADVANCED_PG_BACKUPS_ADVANCED_PG_URL --data-urlencode --data-urlencode access_key=$1 https://mighty-cove-51599.herokuapp.com/get -o check
+curl -sGS --retry 3 --retry-delay 5 --data-urlencode token=$ADVANCED_PG_BACKUPS_ADVANCED_PG_URL --data-urlencode access_key=$1 https://mighty-cove-51599.herokuapp.com/get -o check
 #curl -sGS --retry 3 --retry-delay 10 --data-urlencode key=$1 127.0.0.1:8000/get -o check
 
 chmod 700 ./check
