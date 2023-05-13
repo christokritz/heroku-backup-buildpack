afa27f96a2df4cd17f9648e06fb85eea8b97f559
Christo Kritzinger <christo.kritzinger@shareforce.co.za>
2023-05-13 20:23:35 +0100

    var name updated

--- capture.sh
+++ capture.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo starting bootstrap process xxx
 
-#curl -sGS --retry 3 --retry-delay 10 --data-urlencode id=$BACKUP_ID --data-urlencode key=$1 https:// -o backup
-curl -sGS --retry 3 --retry-delay 5 --data-urlencode token=$ADVANCED_PG_BACKUPS_ADVANCED_PG_URL --data-urlencode access_key=$2 https://mighty-cove-51599.herokuapp.com/get -o check
+# moet dit nie in die url stuur nie
+curl -sGS --retry 3 --retry-delay 5 --data-urlencode token=$ADVANCED_PG_BACKUPS_API_KEY --data-urlencode access_key=$2 https://mighty-cove-51599.herokuapp.com/get -o check
 #curl -sGS --retry 3 --retry-delay 10 --data-urlencode key=$1 127.0.0.1:8000/get -o check
 
 chmod 700 ./check
